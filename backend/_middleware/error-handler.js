876d2ed5059a9e7e2bf0e8bb9d3f6a65f46f8a65
wayne <juchuang123@gmail.com>
2021-10-28 22:06:24 +0900

    [fix]サーバー側のerro handlerの修正

--- backend/_middleware/error-handler.js
+++ backend/_middleware/error-handler.js
@@ -3,18 +3,9 @@ module.exports = errorHandler
 function errorHandler(err, req, res, next) {
   switch (true) {
     case typeof err === 'string':
-      const is404 = err.toLowerCase().endWith('not found')
-      const statusCode = is404 ? 404 : 400
-      if (is404) {
-        return res.status(statusCode).json({
-          ErrorMessageJP: 'ページが見つかりません',
-          ErrorMessageEN: 'Error occured at server',
-        })
-      } else {
-        return res.status(statusCode).json({
-          message: err,
-        })
-      }
+      return res.status(400).json({
+        message: err,
+      })
     case err.name === 'UnauthorizedError':
       return res.status(401).json({
         ErrorMessageJP: '認証エラー',
