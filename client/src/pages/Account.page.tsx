bc6622ceaa86320c939b6cea71bbd436e0874526
wayne <juchuang123@gmail.com>
2021-10-29 12:16:21 +0900

    [add]アカウントページでログアウト機能の呼び出し

--- client/src/pages/Account.page.tsx
+++ client/src/pages/Account.page.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useAuthDispatchContext } from '../context/auth.context'
 
 const Account: React.FC = () => {
+  const { logout } = useAuthDispatchContext()
   return (
     <div>
       <h1>account_config</h1>
+      <button onClick={logout}>サインアウト</button>
     </div>
   )
 }
