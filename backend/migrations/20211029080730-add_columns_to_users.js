b41597f67f64a6e89d834431e50100f3deef544c
wayne <juchuang123@gmail.com>
2021-10-29 20:10:55 +0900

    [add]users tableにカラムの追加

---
+++ backend/migrations/20211029080730-add_columns_to_users.js
@@ -0,0 +1,45 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return Promise.all([
+      queryInterface.addColumn('Users', 'icon', {
+        type: Sequelize.BLOB,
+        defaultValue: null,
+      }),
+      queryInterface.addColumn('Users', 'default_order_price', {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      }),
+      queryInterface.addColumn('Users', 'accepting_order', {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      }),
+      queryInterface.addColumn('Users', 'cash', {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      }),
+    ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return Promise.all([
+      queryInterface.removeColumn('Users', 'icon'),
+      queryInterface.removeColumn('Users', 'default_order_price'),
+      queryInterface.removeColumn('Users', 'accepting_order'),
+      queryInterface.removeColumn('Users', 'cash'),
+    ])
+  },
+}
