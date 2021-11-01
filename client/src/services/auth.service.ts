4163852dae9bb886ef00eb2239f32c60ee1bdc6b
wayne <juchuang123@gmail.com>
2021-11-01 22:34:47 +0900

    [clean]BASE_API_URLをconstantsファイルで一括管理するよう修正

--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import authHeader from './auth-header'
-
-const BASE_API_URL = 'http://localhost:4000'
+import {BASE_API_URL} from '../helper/constants'
 
 interface RegisterCredenntials {
   name: string
