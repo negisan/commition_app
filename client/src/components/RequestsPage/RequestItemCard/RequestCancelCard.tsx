671e68b9121fb250b8c79332fdb85de3ef2a69f4
wayne <juchuang123@gmail.com>
2021-11-03 15:58:53 +0900

    [add]RequestCancelCardComponentの作成

--- client/src/components/RequestsPage/RequestItemCard/RequestCancelCard.tsx
+++ client/src/components/RequestsPage/RequestItemCard/RequestCancelCard.tsx
@@ -1,11 +1,49 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const RequestCancelCard = ({item}:any) => {
-  return (
-    <div>
-      RequestCancelCard
-    </div>
-  )
+import { useRequestsStateContext } from '../../../context/requests.context'
+import { CardHeader, CardWrapper, OrderPrice, OrderContent } from './CardComon'
+
+const RequestCancelCard: React.FC<any> = ({ item }) => {
+  const { role } = useRequestsStateContext()
+
+  if (role === 'client') {
+    return (
+      <CardWrapper>
+        <CardHeader user_name={item.creator.name} />
+        <OrderPrice order_price={item.order_price} />
+        <OrderContent order_content={item.order_content} />
+        <CancelMessageContainer>
+          <p>キャンセルされました</p>
+        </CancelMessageContainer>
+      </CardWrapper>
+    )
+  }
+  if (role === 'creator') {
+    return (
+      <CardWrapper>
+        <CardHeader user_name={item.client.name} />
+        <OrderPrice order_price={item.order_price} />
+        <OrderContent order_content={item.order_content} />
+        <CancelMessageContainer>
+          <p>キャンセルされました</p>
+        </CancelMessageContainer>
+      </CardWrapper>
+    )
+  }
+
+  return <></>
 }
+
+const CancelMessageContainer = styled.div`
+  margin-top: 4rem;
+  display: flex;
+  justify-content: center;
+  p {
+    font-weight: 600;
+    font-size: 1.25rem;
+    color: var(--clr-red);
+  }
+`
 
 export default RequestCancelCard
