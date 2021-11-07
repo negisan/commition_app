bea6fa77b076f330c0fe168893a7fda70b344468
wayne <juchuang123@gmail.com>
2021-11-07 18:06:44 +0900

    [update]UserModelの変更にともなうシードデータの更新

--- backend/src/seeders/20211102102459-create-sample-artworks.js
+++ backend/src/seeders/20211102102459-create-sample-artworks.js
@@ -14,22 +14,67 @@ module.exports = {
      */
     const now = new Date()
     return queryInterface.bulkInsert('Artworks', [
+      // creator user1
       {
         creatorId: 1,
         content: fs
-          .readFileSync(__dirname + '/images/beach1.jpg')
+          .readFileSync(__dirname + '/images/wallpaper1.jpg')
+          .toString('base64'),
+      },
+      {
+        creatorId: 1,
+        content: fs
+          .readFileSync(__dirname + '/images/wallpaper2.jpg')
+          .toString('base64'),
+      },
+      {
+        creatorId: 1,
+        content: fs
+          .readFileSync(__dirname + '/images/wallpaper3.jpg')
+          .toString('base64'),
+      },
+      {
+        creatorId: 1,
+        content: fs
+          .readFileSync(__dirname + '/images/wallpaper4.jpg')
+          .toString('base64'),
+      },
+      {
+        creatorId: 1,
+        content: fs
+          .readFileSync(__dirname + '/images/wallpaper5.jpg')
+          .toString('base64'),
+      },
+
+      // creator user2
+      {
+        creatorId: 2,
+        content: fs
+          .readFileSync(__dirname + '/images/wallpaper6.jpg')
           .toString('base64'),
       },
       {
         creatorId: 2,
         content: fs
-          .readFileSync(__dirname + '/images/beach2.jpg')
+          .readFileSync(__dirname + '/images/wallpaper7.jpg')
           .toString('base64'),
       },
       {
-        creatorId: 1,
+        creatorId: 2,
         content: fs
-          .readFileSync(__dirname + '/images/book1.jpg')
+          .readFileSync(__dirname + '/images/wallpaper8.jpg')
+          .toString('base64'),
+      },
+      {
+        creatorId: 2,
+        content: fs
+          .readFileSync(__dirname + '/images/wallpaper9.jpg')
+          .toString('base64'),
+      },
+      {
+        creatorId: 2,
+        content: fs
+          .readFileSync(__dirname + '/images/wallpaper10.jpg')
           .toString('base64'),
       },
     ])
