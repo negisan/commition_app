6ba3981aed700a0c03b3672f2712bf42766c3209
wayne <juchuang123@gmail.com>
2021-11-11 19:55:50 +0900

    本番環境にURLの切り替え

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+// require('dotenv').config()
 
 const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env
 
--- client/src/helper/constants.ts
+++ client/src/helper/constants.ts
@@ -1,3 +1,3 @@
 export const STRIPE_PUBLISHABLE_KEY = 'pk_test_51IZYWVHGyCEIKqcqt3PTMM7DowDcLglqS2AOI7AU6bXc0B6NfdRD7eFTxjVc38ZNhhApkVEeDtLZo6CTyEPq5wEe00zE2nb4I0'
-export const BASE_API_URL = 'http://localhost:4000'
-// export const BASE_API_URL = 'https://committion-app-api.herokuapp.com'
+// export const BASE_API_URL = 'http://localhost:4000'
+export const BASE_API_URL = 'https://committion-app-api.herokuapp.com'
